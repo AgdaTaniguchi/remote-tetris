918e0f1bfa46debf4a525d1560e778a027a2c52c
AgdaTaniguchi <agda.taniguchi@sptech.school>
2024-08-01 00:26:24 -0300

    feat: backend tetris together

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, disconnect
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,9 +12,9 @@ player_count = 0
 def display():
     return render_template('display.html')
 
-@app.route('/controls')
+@app.route('/controller')
 def control():
-    return render_template('controls.html')
+    return render_template('controller.html')
 
 @socketio.on('connect')
 def connect_handler():
@@ -23,15 +23,16 @@ def connect_handler():
 
 @socketio.on('disconnect')
 def disconnect_handler():
-    print('client disconnected')
+    return
 
 @socketio.on('command')
 def command_handler(command):
     print(command)
-    if command['type'] == 'connected':
-        emit('sendMessageDisplay', {'nick': command['nick'], 'status': 'connected'}, broadcast=True)
-    elif command['type'] == 'movement':
-        emit('sendMessageDisplay', {'nick': command['nick'], 'movement': command['movement']}, broadcast=True)
+    emit('sendMessageDisplay', command, broadcast=True)
+    # if command['type'] == 'connected':
+        # emit('sendMessageDisplay', {'nick': command['nick'],  'status': 'connected'}, broadcast=True)
+    # elif command['type'] == 'movement':
+    #     emit('sendMessageDisplay', {'nick': command['nick'], 'movement': command['movement']}, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8765, debug=True)
